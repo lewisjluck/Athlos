f94025bf4d74dfa4c0e60749735e71e47874910f
lewisjluck <lewisjluck@gmail.com>
2018-09-02 09:29:16 +1000

    Initial Commit

---
+++ SportScore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SportScore
+//
+//  Created by Lewis Luck on 2/9/18.
+//  Copyright © 2018 Fortune Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
