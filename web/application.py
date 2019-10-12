7ca1141db2a269c907bb7911ce7404220f2c2348
Lewis Luck <lewisjluck@gmail.com>
2019-10-12 23:02:37 +1000

    Added flask, cosmetic updates

---
+++ web/application.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template, redirect, flash, session, request, jsonify
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+app = Flask(__name__)
