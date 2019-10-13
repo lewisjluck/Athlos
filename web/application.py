460f64e8e62a40d89cc0bc6f0ef5262623fdc8c5
Lewis Luck <lewisjluck@gmail.com>
2019-10-13 20:28:33 +1000

    Continued customization and application

--- web/application.py
+++ web/application.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, redirect, flash, session, request, jsonify
-from flask_session import Session
-from tempfile import mkdtemp
-from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
-from werkzeug.security import check_password_hash, generate_password_hash
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+app.secret_key = 'ulaanbaataar'
+
+@app.route("/")
+def index():
+    return render_template("homepage.html")
